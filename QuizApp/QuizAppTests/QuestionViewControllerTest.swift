059c03113990737ab153c01edf38c9aeea694e55
Erva Hatun Tekeoğlu <eherken@gmail.com>
2023-08-03 18:08:35 +0300

    Question view controller test has been started.

---
+++ QuizApp/QuizAppTests/QuestionViewControllerTest.swift
@@ -0,0 +1,40 @@
+//
+//  QuestionViewControllerTest.swift
+//  QuizAppTests
+//
+//  Created by Erva Hatun Tekeoğlu on 3.08.2023.
+//
+
+import Foundation
+import XCTest
+@testable import QuizApp
+
+class QuestionViewControllerTest: XCTestCase {
+    func test_viewDidLoad_renderQuestionHeaderText() {
+        let sut = QuestionViewController(question: "Q1",options: [])
+        _ = sut.view
+        XCTAssertEqual(sut.headerLabel.text, "Q1")
+    }
+    
+    func test_viewDidLoad_withNoOptions_rendersZeroOptions() {
+        let sut = QuestionViewController(question: "Q1", options: [])
+        _ = sut.view
+        XCTAssertEqual(sut.tableView.numberOfRows(inSection: 0), 0)
+    }
+    
+    func test_viewDidLoad_withoneOption_rendersOneOption() {
+        let sut = QuestionViewController(question: "Q1", options: ["A1"])
+        _ = sut.view
+        XCTAssertEqual(sut.tableView.numberOfRows(inSection: 0), 1)
+    }
+    
+    func test_viewDidLoad_withoneOption_rendersOneOptionText() {
+        let sut = QuestionViewController(question: "Q1", options: ["A1"])
+        _ = sut.view
+        
+        let indexPath = IndexPath(row: 0, section: 0)
+        let cell = sut.tableView.dataSource?.tableView(sut.tableView, cellForRowAt: indexPath)
+        XCTAssertEqual(cell?.textLabel?.text, "A1")
+    }
+
+}
