30a0fdc4d7ed953bfd86bc4116bf7d19b453e2e5
Erva Hatun Tekeoğlu <eherken@gmail.com>
2023-08-04 12:22:58 +0300

    VC is created and unit test functions are added.

--- QuizApp/QuizApp/AppDelegate.swift
+++ QuizApp/QuizApp/AppDelegate.swift
@@ -10,10 +10,13 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        window.rootViewController = QuestionViewController(question: "A guestion?", options: ["Option 1", "Option 2"]) {  print($0)}
+        window.makeKeyAndVisible()
+        self.window = window
         return true
     }
 
